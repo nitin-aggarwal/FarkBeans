11ea0d765eb6ba5f14776fcc479d5be631890ed5
Nitin Aggarwal <nitin3588.aggarwal@gmail.com>
2012-04-09 22:14:00 -0400

    query Changes

--- src/services/RetrieveDataSrv.java
+++ src/services/RetrieveDataSrv.java
@@ -3,7 +3,6 @@ package services;
 import java.util.List;
 
 import javax.persistence.Query;
-
 import entities.AbstractDB;
 
 
@@ -55,7 +54,7 @@ public class RetrieveDataSrv extends BaseSrv {
        System.out.println(tagConditions);
        
        query = getEntityManger().createQuery("Select st FROM "+ tableName+" st WHERE st.farkTag IN ("+tagConditions+
-    		   ") and st.articleContent is NOT NULL and st.id <= 10");
+    		   ") and st.articleContent is NOT NULL");
        //query.setMaxResults(1);
        
        @SuppressWarnings("unchecked")
